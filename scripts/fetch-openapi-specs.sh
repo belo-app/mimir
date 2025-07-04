97a8e99eef7b73d075dc50ee00f34cddc25c6437
Ignacio Norris Perret <nacho@belo.app>
2025-07-04 22:32:42 +0200

    remove frigg openapi fetch

--- scripts/fetch-openapi-specs.sh
+++ scripts/fetch-openapi-specs.sh
@@ -8,7 +8,6 @@ curl --fail --retry 5 --retry-delay 5 https://api.stg.belo.app/v1/docs/json -o s
 curl --fail --retry 5 --retry-delay 5 https://api.stg.belo.app/v2/docs/json -o static/openapi/v2.json
 curl --fail --retry 5 --retry-delay 5 https://api.stg.belo.app/payment/docs/json -o static/openapi/payment.json
 curl --fail --retry 5 --retry-delay 5 https://api.stg.belo.app/v1/docs/json -o static/openapi/_v3.json
-curl --fail --retry 5 --retry-delay 5 https://frigg.stg.belo.link/v1/docs/json -o static/openapi/crypto.json
 curl --fail --retry 5 --retry-delay 5 https://api.stg.baldr.app/api/docs/json -o static/openapi/backoffice/api.json
 curl --fail --retry 5 --retry-delay 5 https://api.stg.baldr.app/dashboard/docs/json -o static/openapi/backoffice/dashboard.json
 curl --fail --retry 5 --retry-delay 5 https://api.stg.baldr.app/public/docs/json -o static/openapi/backoffice/public.json
